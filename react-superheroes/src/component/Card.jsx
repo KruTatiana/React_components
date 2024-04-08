fcb9c9d1e818eabe250967cf8531d180fbd25524
KruTatiana <ilm63.rus@gmail.com>
2024-04-08 11:21:09 +0300

    destructuring in Card

--- react-superheroes/src/component/Card.jsx
+++ react-superheroes/src/component/Card.jsx
@@ -1,14 +1,23 @@
-function Card(props) {
+function Card({
+  name,
+  universe,
+  alterego,
+  occupation,
+  friends,
+  superpowers,
+  info,
+  url
+}) {
     return(
       <div className="card">
-        <h3 className="card_name">{props.name}</h3>
-        <div className="card_universe">Вселенная: {props.universe}</div>
-        <div className="card_alterego">Альтер эго: {props.alterego}</div>
-        <div className="card_occupation">Род деятельности: {props.occupation}</div>
-        <div className="card_friends">Друзья:{props.friends}</div>
-        <div className="card_superpowers">Суперсилы: {props.superpowers}</div>
-        <div className="card_info">{props.info}</div>
-        <img src={props.url} alt={props.name} className="card_img" />
+        <h3 className="card_name">{name}</h3>
+        <div className="card_universe">Вселенная: {universe}</div>
+        <div className="card_alterego">Альтер эго: {alterego}</div>
+        <div className="card_occupation">Род деятельности: {occupation}</div>
+        <div className="card_friends">Друзья:{friends}</div>
+        <div className="card_superpowers">Суперсилы: {superpowers}</div>
+        <div className="card_info">{info}</div>
+        <img src={url} alt={name} className="card_img" />
       </div>
     );
   };
